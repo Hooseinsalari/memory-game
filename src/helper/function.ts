49ae23ac0f8d040cae10eb49a10ed62ad83196a3
Hossein Salari <hossein.salari002@gmail.com>
2023-08-03 17:22:13 +0330

    create a function for convert time

--- src/helper/function.ts
+++ src/helper/function.ts
@@ -9,4 +9,12 @@ const shuffleArray = <T>(array: T[]): T[] => {
   return newArray;
 };
 
-export { shuffleArray };
+const convertTime = (seconds: number): string => {
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = seconds % 60;
+  const minutesString = String(minutes).padStart(2, '0');
+  const secondsString = String(remainingSeconds).padStart(2, '0');
+  return `${minutesString}:${secondsString}`;
+};
+
+export { shuffleArray, convertTime };
